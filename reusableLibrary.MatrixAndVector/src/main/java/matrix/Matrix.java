2844ce1897ff3f0124d62dea37f00925a53bf143
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2020-06-02 18:50:41 +0200

    Special constructor, getter and setter, equality

--- reusableLibrary.MatrixAndVector/src/main/java/matrix/Matrix.java
+++ reusableLibrary.MatrixAndVector/src/main/java/matrix/Matrix.java
@@ -1,12 +1,15 @@
 package matrix;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 
 import Exception.*;
 
 public class Matrix {
 	private BigDecimal[][] matrix; // a two dimensional table
 	private int[] shape; // i=0 is the rows' number ; i=1 is the columns' number
+	
 	
 	
 //===============CONSTRUCTORS==================
@@ -27,9 +30,38 @@ public class Matrix {
 		this.verifyEntry(matrix);
 		this.matrix = matrix;
 		this.initializeShape();
+	} 
+	
+
+	//===============SPECIAL MATRIX===============
+	
+	// return a blank matrix
+	public Matrix(int m, int n) {
+		this.matrix = new BigDecimal[m][n];
+		
+		// for each rows
+		for (int i = 0 ; i < m ; i++) {
+			// for each columns
+			for (int j = 0 ; j < n ; j++) {
+				this.setValueAt(i, j, 0); 
+			}
+		}
+		this.initializeShape();
 	}
-
-
+	
+	// return the identity matrix
+	public static Matrix identity(int size) {
+		Matrix identity = new Matrix(size, size);
+		
+		for (int i = 0 ; i < size ; i++) {
+			for (int j = 0 ; j < size ; j++) {
+				if (i==j) {
+					identity.setValueAt(i, j, 1);
+				}
+			}
+		}
+		return identity;
+	}
 	
 	
 //===============CONSTRUCTORS HELPERS=================
@@ -116,14 +148,15 @@ public class Matrix {
 
 	
 	
+	
 //==============GETTERS AND SETTERS================
 	
-	public int[] getTableShape() {
+	public int[] getShape() {
 		return this.shape;
 	}
 	
 	// return a string which contains the shape
-	public String getShape() {
+	public String getStringShape() {
 		return "("+ this.shape[0] + ", " + this.shape[1] + ")";
 	}
 	
@@ -137,6 +170,97 @@ public class Matrix {
 		return this.shape[1];
 	}
 	
+	// return the at the row m and the column n
+	public BigDecimal getValueAt(int m, int n) {
+		return this.matrix[m][n];
+	}
+	
+	// modify the value at the row m and the column n
+	public void setValueAt(int m, int n, int value) {
+		this.matrix[m][n] = new BigDecimal(value);
+	}
+	
+	// modify the value at the row m and the column n
+	public void setValueAt(int m, int n, double value) {
+		this.matrix[m][n] = new BigDecimal(value);
+	}
+	
+	// modify the value at the row m and the column n
+	public void setValueAt(int m, int n, BigDecimal value) {
+		this.matrix[m][n] = value;
+	}
+	
+	// display of the matrix
+	public String toString() {
+		String info = "";
+		DecimalFormat df = new DecimalFormat("###.########");
+		
+		for (int i = 0 ; i < m() ; i++) {
+			info+="| ";
+			for (int j = 0 ; j < n() ; j++) {
+				BigDecimal value = getValueAt(i, j);
+				if (value.compareTo(BigDecimal.ZERO)>=0) {
+					info+=" ";
+				}
+				info+=df.format(value.doubleValue())+" ";
+			}
+			info+="|\n";
+		}
+		return info;
+	}
+	
+	
+	
+//=============COMPARISON=============
+	
+	// verify if the two matrixes have the same shape, if not, throw an exception
+	private void verifySameShape(Matrix matrix2) throws WrongShapeException {
+		if (!Arrays.equals(this.getShape(),matrix2.getShape())) {
+			throw new WrongShapeException("The two matrixes don't have the same shape "+
+					this.getStringShape()+" != " + matrix2.getStringShape());
+		}
+	}
+	
+	// test if all the values of the matrix are same as the other
+	public boolean equals(Matrix matrix2) throws WrongShapeException {
+		verifySameShape(matrix2);
+		
+		for (int i = 0 ; i < this.m() ; i++) {
+			for (int j = 0 ; j < this.n() ; j++) {
+				if (!this.getValueAt(i, j).equals(matrix2.getValueAt(i, j))) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	
+	
+//===============OPERATIONS===============
+	
+	// a matrix plus a matrix
+	public Matrix add(Matrix matrix2) {
+		return null;
+	}
+	
+	
+	// a matrix minus a matrix
+	public Matrix subtract(Matrix matrix2) {
+		return null;
+	}
+	
+	
+	// a matrix multiply by a matrix
+	public Matrix multiply(Matrix matrix2) {
+		return null;
+	}
+	
+	
+	// transpose of the matrix
+	public Matrix T() {
+		return null;
+	}
 	
 	
 }
--- reusableLibrary.MatrixAndVector/src/test/java/reusableLibrary/MatrixAndVector/MatrixTest.java
+++ reusableLibrary.MatrixAndVector/src/test/java/reusableLibrary/MatrixAndVector/MatrixTest.java
@@ -2,11 +2,14 @@ package reusableLibrary.MatrixAndVector;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import Exception.MatrixMisconstructionException;
+import Exception.WrongShapeException;
 import matrix.Matrix;
 
 public class MatrixTest {
@@ -31,7 +34,7 @@ public class MatrixTest {
 	public void matrix23_should_return_23shape() {
 		int[] result = new int[] {2, 3};
 		
-		assertArrayEquals(result, matrix2.getTableShape());
+		assertArrayEquals(result, matrix2.getShape());
 	}
 	
 	
@@ -46,5 +49,48 @@ public class MatrixTest {
 		Matrix badMatrix = new Matrix(new int[][] {{1, 2},
 													{2, 4, 5}});
 	}
+	 
+	@Test
+	public void test_equality_should_return_true() throws MatrixMisconstructionException, WrongShapeException {
+		Matrix test = new Matrix(new int[][]{ {1, 2, 5},
+											{-1, 5, -7},
+											{-8, 7, 11}} );
+		assertTrue(matrix1.equals(test));
+	}
+	
+	@Test
+	public void test_equality_should_return_false() throws MatrixMisconstructionException, WrongShapeException {
+		Matrix test = new Matrix(new int[][]{ {-1, 2, 5},
+											{-1, 5, -7},
+											{-8, 7, 11}} );
+		assertFalse(matrix1.equals(test));
+	}
+	
+	@Test(expected = WrongShapeException.class)
+	public void test_equality_should_trows_exception() throws WrongShapeException {
+		matrix1.equals(matrix2);
+	}
+	
+	@Test
+	public void modify_value() throws WrongShapeException, MatrixMisconstructionException {
+		Matrix test = new Matrix(new int[][] {{1, -5, 3},
+			  					   				{2, -5, 7}});
+		
+		test.setValueAt(0, 1, 2);
+		
+		assertTrue(matrix2.equals(test));
+	}
+	
+	@Test
+	public void test_identity() throws MatrixMisconstructionException, WrongShapeException {
+		Matrix identity3 = new Matrix(new int[][] {{1, 0, 0},
+													{0, 1, 0},
+													{0, 0, 1}});
+		
+		assertTrue(identity3.equals(Matrix.identity(3)));
+		
+	}
+	
+	
 	
 }
